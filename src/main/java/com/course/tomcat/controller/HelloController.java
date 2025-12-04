ed8be05ab598be17d599c206aae81bc5bf12673e
pavi2527 <pavithra.k3@dxc.com>
2025-12-04 13:38:57 +0530

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -17,10 +17,5 @@ public class HelloController {
     int random(){
         return ThreadLocalRandom.current().nextInt(0,1000);
     }
-    @GetMapping(value = "/hero")
-    String hero(){
-        return "My favorite superhero is Batman";
-    }
-
  
 }
